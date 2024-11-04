d61b1fba662b4a66bd2c4452323701b35c606766
Foxrim <Flavien085@gmail.com>
2024-11-04 19:32:43 +0100

    [feat] : Ajout page personnel

---
+++ client/src/pages/Flavien.tsx
@@ -0,0 +1,11 @@
+export default function Flavien() {
+
+    return (
+        <>
+            <h2>Wow c'est génial ici</h2>
+            <h3>Wow c'est génial ici</h3>
+            <h4>Wow c'est génial ici</h4>
+            <p>OUAIS OUAIS OUAIS !</p>
+        </>
+    )
+}
